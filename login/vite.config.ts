bd7fc510ffd0fd1a235e82f105dcb5a4601ddb41
gabrielvn05 <gabrielalexanderveleznunez@gmail.com>
2025-08-04 01:26:49 -0500

    arreglos de vercel

--- login/vite.config.ts
+++ login/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+   build: {
+    outDir: 'dist', // Asegúrate de que los archivos de salida estén en dist
+    rollupOptions: {
+      input: '/index.html', // Asegúrate de que el archivo HTML sea el correcto
+    },},
   server: {
     proxy: {
       // Redirigir las solicitudes a tu backend de Render
